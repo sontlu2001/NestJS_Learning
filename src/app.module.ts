c37472fbb21edd6bed5c03e74f1c09254b6e6960
Do Ngoc Son <sondo1@cqtdt.asia>
2025-03-27 17:32:00 +0700

    CRUD with Pisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { ProductsModule } from './modules/products/products.module';
-import { Product } from './entities/Product';
+import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5433,
-      username: 'postgres',
-      password: '12344321',
-      database: 'nest_basic',
-      entities: [Product],
-      synchronize: true,
-    }),
-    ProductsModule,
+    PrismaModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
